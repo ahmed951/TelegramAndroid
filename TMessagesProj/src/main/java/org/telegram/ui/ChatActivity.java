122f2c67d40af88c58dee1b5c0aa964b35da0533
Ahmed Alaa <42003517+ahmed951@users.noreply.github.com>
2025-10-26 16:31:48 +0300

    disable open chat with username

--- TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
@@ -35941,6 +35941,8 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
     private void openClickableLink(CharacterStyle url, String str, boolean longPress, final ChatMessageCell cell, final MessageObject messageObject, boolean forceNoIV) {
         if (longPress) {
             if (str.startsWith("@")) {
+                Toast.makeText(getParentActivity(), "Username links are disabled", Toast.LENGTH_SHORT).show();
+                return;
                 if (cell != null) {
                     cell.resetPressedLink(-1);
                 }
@@ -36059,7 +36061,9 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
                         shakeContent();
                     }
                 } else if (str.startsWith("@")) {
-                    getMessagesController().openByUserName(username, ChatActivity.this, 0, makeProgressForLink(cell, url));
+                   // getMessagesController().openByUserName(username, ChatActivity.this, 0, makeProgressForLink(cell, url));
+                    Toast.makeText(getParentActivity(), "Username links are disabled", Toast.LENGTH_SHORT).show();
+                    return;
                 } else {
                     processExternalUrl(0, str, url, cell, false, false);
                 }
